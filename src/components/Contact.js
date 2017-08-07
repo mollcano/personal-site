6e93fe87b4bb56cb181943767a4bf2bf90079385
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-08-07 14:08:53 -0600

    added thumbnail images for projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,19 +5,21 @@ export default class Contact extends Component{
   render(){
     return(
         <div className="container-fluid col-md-12">
-          <div className="col-md-3">
-            <img src="./images/github.png" alt="github link" />
+          <div className="links">
+
+              <a target="_blank" href="https://github.com/mollcano"><img  className="contact" src="./images/github.png" alt="github link" /></a>
+
+
+              <a target="_blank" href="https://www.linkedin.com/in/mollcano/"><img className="contact" src="./images/linkedin.png" alt="linkedin link" /></a>
+
+
+              <a href="mailto:molly@mollcano.com"><img className="contact" src="./images/mail.png" alt="link to email" /></a>
+
+
+              <a href="tel:+01-217-246-8502"><img className="contact" src="./images/phone.png" alt="phone number" /></a>
+          
           </div>
-          <div className="col-md-3">
-            <img src="./images/linkedin.png" alt="linkedin link" />
-          </div>
-          <div className="col-md-3">
-            <img src="./images/mail.png" alt="link to email" />
-          </div>
-          <div className="col-md-3">
-            <img src="./images/phone.png" alt="phone number" />
-          </div>
-          </div>
+        </div>
 
     )
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,34 +5,57 @@ export default class Header extends Component{
   render(){
     return(
       <div className="row">
-      <div className="container-fluid col-md-8">
-
-        <div className="col-md-4">
-        <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div className="col-md-4">
-        <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div className="col-md-4">
-        <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div className="col-md-4">
-        <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div className="col-md-4">
-        <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div className="col-md-4">
-        <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+        <div className="container-fluid col-md-8">
+          <div className="col-md-4">
+            <a href="#" class="thumbnail">
+              <img className="projectPic" src="./images/MOLLCANO.png" alt="..." />
+            </a>
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <a href="#" class="thumbnail">
+              <img className="projectPic" src="./images/MOLLCANO.png" alt="..." />
+            </a>
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <a href="#" class="thumbnail">
+              <img className="projectPic" src="./images/MOLLCANO.png" alt="..." />
+            </a>
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <a href="#" class="thumbnail">
+              <img className="projectPic" src="./images/MOLLCANO.png" alt="..." />
+            </a>
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <a href="#" class="thumbnail">
+              <img className="projectPic" src="./images/MOLLCANO.png" alt="..." />
+            </a>
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>  
         </div>
       </div>
+
+
+
     )
   }
 }
