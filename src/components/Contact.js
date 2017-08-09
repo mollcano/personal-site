31727984beb4a2050bbbe660a699d2c804523c87
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-08-09 10:39:52 -0600

    final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,17 +7,17 @@ export default class Contact extends Component{
         <div className="container-fluid col-md-12">
           <div className="links">
 
-              <a target="_blank" href="https://github.com/mollcano"><img  className="contact" src="./images/github.png" alt="github link" /></a>
+              <a target="blank" href="https://github.com/mollcano"><img  className="contact" src="./images/github.png" alt="github link" /></a>
 
 
-              <a target="_blank" href="https://www.linkedin.com/in/mollcano/"><img className="contact" src="./images/linkedin.png" alt="linkedin link" /></a>
+              <a target="blank" href="https://www.linkedin.com/in/mollcano/"><img className="contact" src="./images/linkedin.png" alt="linkedin link" /></a>
 
 
-              <a href="mailto:molly@mollcano.com"><img className="contact" src="./images/mail.png" alt="link to email" /></a>
+              <a target="blank" href="mailto:molly@mollcano.com"><img className="contact" src="./images/mail.png" alt="link to email" /></a>
 
 
-              <a href="tel:+01-217-246-8502"><img className="contact" src="./images/phone.png" alt="phone number" /></a>
-          
+              <a target="blank" href="https://drive.google.com/file/d/0B8SjphBDghvYS1c5S2NfbVRZNUk/view?usp=sharing"><img className="contact" src="./images/resume.png" alt="resume" /></a>
+
           </div>
         </div>
 
