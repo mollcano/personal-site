94df504ef5429a4682e478951e46a445397d64fb
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-08-09 08:26:29 -0600

    added about me

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ export default class About extends Component{
       <div className="container-fluid col-md-3 about">
         <h3 className="aboutMe">About Me</h3>
         <img className="myPic" src="./images/MollyPic.jpg" alt="Molly" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>I'm a psychologist turned web developer. I enjoy solving new problems and coming up with ways to make my life and other's lives easier. My passion for people transfers nicely into developing products with users in mind. I love the mix of developing a simple, yet beautiful front-end site with the exciting problem solving aspects of developing the back-end.</p>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Learning new things keeps my fire for coding alive, and, luckily, in web development, there is always something new to learn.  When I'm not programming, you can find me in the mountains exploring new territory, or just on my couch watching Netflix and eating pizza.  Now that you know more about me, check out my projects, or if you are really interested, shoot me an email.</p>
         </div>
     )
   }
